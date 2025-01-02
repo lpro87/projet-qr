728e7018aadc9b195521cb1c775ff8625b1bea87
lpro87 <loicpro@gmail.com>
2025-01-02 23:26:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Page chargée, en attente...");
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD09ASJgRUtalU7mJ8a9s3Lw4OBGhJxAPE",
+    authDomain: "clisson-2025.firebaseapp.com",
+    databaseURL: "https://clisson-2025-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "clisson-2025",
+    storageBucket: "clisson-2025.firebasestorage.app",
+    messagingSenderId: "980033637644",
+    appId: "1:980033637644:web:67ed1786e3be93187f0d29"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
